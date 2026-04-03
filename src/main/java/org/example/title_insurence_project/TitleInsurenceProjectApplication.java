e9c0f04f6ed1fd1ef72977fdd10c431013dc6826
Venu <venubade3421@gmail.com>
2026-04-03 10:40:22 +0530

    title insurence project started

---
+++ src/main/java/org/example/title_insurence_project/TitleInsurenceProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.title_insurence_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitleInsurenceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitleInsurenceProjectApplication.class, args);
+    }
+
+}
